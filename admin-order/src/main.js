807102638705255003432ca01ad6a2edca04d2c3
ann8355 <s8900771@gmail.com>
2018-09-06 23:33:10 +0800

    commit setting

--- admin-order/src/main.js
+++ admin-order/src/main.js
@@ -6,7 +6,6 @@ import router from './router'
 import 'bootstrap'
 import 'bootstrap/dist/css/bootstrap.css'
 import fontawesome from '@fortawesome/fontawesome'
-import FontAwesomeIcon from '@fortawesome/vue-fontawesome'
 import solid from '@fortawesome/fontawesome-free-solid'
 import regular from '@fortawesome/fontawesome-free-regular'
 import brands from '@fortawesome/fontawesome-free-brands'
@@ -15,7 +14,6 @@ fontawesome.library.add(solid) // Use any icon from the Solid style
 fontawesome.library.add(regular)
 fontawesome.library.add(brands)
 
-Vue.component('font-awesome-icon', FontAwesomeIcon)
 Vue.use(require('vue-faker'))
 
 Vue.config.productionTip = false
