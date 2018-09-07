00b4bbdcbccc17727102ae6af0fee0b9c331f3fe
陳姵安(Ann Chen) <1607012@iisigroup.com>
2018-09-07 18:40:04 +0800

    modify modal

--- admin-order/src/components/Modal.vue
+++ admin-order/src/components/Modal.vue
@@ -4,14 +4,16 @@
         <div class="modal-content shadow">
             <div class="modal-header text-white bg-dark px-4">
                 <h4 class="modal-title">ADD NEW PRODUCT</h4>
-                <button type="button" class="close text-white" data-dismiss="modal">&times;</button>
+                <button type="button" class="close text-white" data-dismiss="modal" v-on:click="clear()">&times;</button>
             </div>
+          <form ref="productForm" v-on:submit.prevent>
             <div class="modal-body d-flex pt-4 px-4">
                 <div class="col">
-                  <div id="upload" class="d-flex flex-column justify-content-center align-items-center" style="height:115px;">
+                  <div id="upload" @click="$refs.files.click()" class="d-flex flex-column justify-content-center align-items-center" style="height:115px;">
                     <i class="fas fa-cloud-upload-alt fa-3x mb-2"></i>
-                    <h6>Drag an image or click here to upload…</h6>
+                    <h6>Click here to upload images</h6>
                   </div>
+                  <input ref="files" @change="previewFiles" multiple class="d-none" type="file" accept="image/*" required>
                   <div id="uploadImg" class="d-flex flex-wrap mt-3">
                     <!-- <img height="126" class="col-4" src="https://images.unsplash.com/photo-1536007164800-b7f11331f35c?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=2fea553f92753fc4768ec28d8c619968&auto=format&fit=crop&w=1050&q=80" alt="img"> -->
                   </div>
@@ -27,13 +29,13 @@
                       <div class="input-group-prepend">
                         <span class="input-group-text">Original</span>
                       </div>
-                      <input type="text" class="form-control" required v-model="original">
+                      <input type="number" class="form-control" required v-model.number="original">
                     </div>
                     <div class="input-group">
                       <div class="input-group-prepend">
                         <span class="input-group-text">Discount</span>
                       </div>
-                      <input type="text" class="form-control" v-model="discount" required>
+                      <input type="number" class="form-control" v-model.number="discount" required>
                     </div>
                   </div>
                   <h6>Specification</h6>
@@ -43,7 +45,7 @@
                       <div class="input-group-prepend">
                         <span class="input-group-text">Size</span>
                       </div>
-                      <select class="custom-select" v-model="item.sizeSelected">
+                      <select class="custom-select" v-model="item.sizeSelected" required>
                         <option v-for="(item2) in item.size" :key="item2.id" v-bind:value="item2">{{item2}}</option>
                       </select>
                     </div>
@@ -57,16 +59,17 @@
                       <div class="input-group-prepend">
                         <span class="input-group-text">Inventory</span>
                       </div>
-                      <input type="text" class="form-control" v-model="item.inventory" required>
+                      <input type="number" class="form-control" v-model.number="item.inventory" required>
                     </div>
                   </div>
                   <button type="button" v-on:click="addSpecificate()" class="btn btn-secondary btn-block mt-3" >ADD NEW SPECIFICATION<i class="fa fa-plus ml-2"></i></button>
-              </div>
+                </div>
             </div>
             <div class="modal-footer pb-4" style="padding-right:39px;">
-                <button type="submit" v-on:click="save()" class="btn btn-light">SAVE DRAFT</button>
-                <button type="button" v-on:click="publish()" class="btn btn-secondary" >PUBLISH</button>
+                <button type="submit" ref="saveDraft" v-on:click="save('saveDraft')" class="btn btn-light">SAVE DRAFT</button>
+                <button type="submit" ref="publish" v-on:click="save('publish')" class="btn btn-secondary">PUBLISH</button>
             </div>
+          </form>
         </div>
     </div>
 </div>
@@ -75,43 +78,147 @@
 export default {
   name: 'Modal',
   strict: true,
+  computed: {
+    discount: {
+      get () {
+        this.discounts = Math.round(this.original*0.8)
+        return Math.round(this.original*0.8)
+      },
+      set (value) {
+        this.discounts = value
+      }
+    }
+  },
   methods: {
     addSpecificate() {
-        const obj = {
-            size: ['Choose Size','S','M','L'],
-            sizeSelected: 'Choose Size',
-            color: '',
-            inventory: ''
-        }
-        this.specificates.push(obj)
+      let obj =  {
+          size: ['','S','M','L'],
+          sizeSelected: '',
+          color: '',
+          inventory: null
+      }
+      this.specificates.push(obj)
     },
     minusSpecificate(index) {
-        this.specificates.splice(index, 1)
+      this.specificates.splice(index, 1)
+    },
+    clear(){
+      this.productName = ''
+      this.proDiscript = ''
+      this.original = null
+      this.discounts = null
+      this.file = []
+      this.status = null
+      this.specificates = [
+        {
+          size: ['','S','M','L'],
+          sizeSelected: '',
+          color: '',
+          inventory: null
+        },
+        {
+          size: ['','S','M','L'],
+          sizeSelected: '',
+          color: '',
+          inventory: null
+        }
+      ]
+    },
+    save(name) {
+      let id;
+      if(name == 'saveDraft'){
+        id = this.$refs.saveDraft
+        this.status = ['btn-secondary','Unpublished','2']
+      }else if(name == 'publish'){
+        id = this.$refs.publish
+        this.status = ['btn-success', 'Published','1']
+      }
+      id.removeAttribute("data-dismiss")
+      if (this.$refs.productForm.checkValidity()) {
+        console.log(this.productName)
+        console.log(this.proDiscript)
+        console.log(this.original)
+        console.log(this.discounts)
+        console.log(this.specificates)
+        console.log(this.status)
+        console.log(this.file)
+        id.setAttribute("data-dismiss", "modal")
+      }
+      this.clear()
+    },
+    previewFiles() {
+      this.file = this.$refs.files.files
     }
   },
   data () {
     return {
       productName: '',
       proDiscript: '',
-      original: '',
-      discount: '',
+      original: null,
+      discounts: null,
+      file: [],
+      status: null,
       specificates: [
-          {
-              size: ['Choose Size','S','M','L'],
-              sizeSelected: 'Choose Size',
-              color: '',
-              inventory: ''
-          },
-          {
-              size: ['Choose Size','S','M','L'],
-              sizeSelected: 'Choose Size',
-              color: '',
-              inventory: ''
-          }
+        {
+          size: ['','S','M','L'],
+          sizeSelected: '',
+          color: '',
+          inventory: null
+        },
+        {
+          size: ['','S','M','L'],
+          sizeSelected: '',
+          color: '',
+          inventory: null
+        }
       ]
     }
   }
 }
+// $('#file').change(function(event) {
+//   fileArray.length = 0;
+//   $("#profile").html("");
+//   var files = document.getElementById('file').files;
+//  // fileArray = Array.prototype.slice.call(files);//無法直接刪除files,須先複製一份array
+//   if(files.length >3){
+//     $("#warnpic").removeClass("d-none");
+//     $("#warnpic span").text("EXEED 3 PHOTOS");
+//     $('#file').val("");
+//     $("#picBtn").attr("disabled",true);
+//   }else{
+//     $("#picBtn").attr("disabled",true);
+//     $("#warnpic").addClass("d-none");
+//     for (var i = 0; i < files.length; i++){
+//       var index = 0;
+//       var file = files[i];
+//       var name = file.name;//檔案名稱
+//       var type = file.type;//檔案類型
+//       var size = file.size;//檔案大小
+//       var reader = new FileReader();
+//       reader.readAsDataURL(file);//轉換成圖片url
+//       // reader.readAsText(file,"UTF-8");//轉換成檔案內容文字,預設編碼是UTF-8(word需存成txt檔)
+//       reader.onload = function(e){
+//         var fileContent = e.target.result;
+//         var image = new Image();      
+//         image.onload=function(){//要在 image.src = 之前cached
+//           var width = image.width;
+//           var height = image.height;
+//           if(width >800 || height>800){
+//             $("#warnpic").removeClass("d-none");
+//             $("#warnpic span").text(name).append("<p>IS OVER THE MAXIMUM SIZE</p>");
+//             // $("#warnpic span").text("ONE FILE IS OVER THE MAXIMUM SIZE");
+//           }else{
+//             fileArray.push(file);
+//             $("#profile").append('<div class="col-4 mr-3 mb-4" data-index='+index+' style="background-image:url('+fileContent+');"></div>');
+//             index++;
+//             $("#picBtn").attr("disabled",false);
+//           }
+//         }; 
+//         image.src= fileContent;  
+//       };
+//     }
+//   }
+// });
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
