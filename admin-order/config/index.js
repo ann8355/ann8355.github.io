de8ab742624baaf053863adc4c08042ba58859a9
ann8355 <s8900771@gmail.com>
2020-01-08 17:02:07 +0800

    modify admin-order assetPublicPath

--- admin-order/config/index.js
+++ admin-order/config/index.js
@@ -50,7 +50,7 @@ module.exports = {
     // Paths
     assetsRoot: path.resolve(__dirname, '../dist'),
     assetsSubDirectory: 'static',
-    assetsPublicPath: '/',
+    assetsPublicPath: '/ann8355.github.io/',
 
     /**
      * Source Maps
