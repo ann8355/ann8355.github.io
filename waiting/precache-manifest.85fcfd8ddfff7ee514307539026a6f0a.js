1edc0dbfd3f69e10f9523251aef80732c900c3af
ann8355 <s8900771@gmail.com>
2020-01-09 00:55:27 +0800

    modify

---
+++ waiting/precache-manifest.85fcfd8ddfff7ee514307539026a6f0a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/waiting/robots.txt"
+  },
+  {
+    "revision": "9e86c9cde071a29880c8",
+    "url": "/waiting/public/js/chunk-vendors.046d8716.js"
+  },
+  {
+    "revision": "03adf36070236fc26043",
+    "url": "/waiting/public/js/app.a412b34c.js"
+  },
+  {
+    "revision": "03adf36070236fc26043",
+    "url": "/waiting/public/css/app.83372263.css"
+  },
+  {
+    "revision": "72e58adb014cd4827b9d5d05671e7dc6",
+    "url": "/waiting/index.html"
+  },
+  {
+    "revision": "210c69dc9c1da3a2726d304a4f5493e9",
+    "url": "/waiting/img/logo/YAKINIKULIKE_TAIWAN.jpg"
+  },
+  {
+    "revision": "72d730db57cd516a947867489b16013c",
+    "url": "/waiting/img/logo/ANNE.jpg"
+  }
+];
